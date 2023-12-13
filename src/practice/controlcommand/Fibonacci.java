b19158b3f47383bc293442eb1f8611841654e31a
Dang-Thi-My-Hanh <22130072@st.hcmuaf.edu.vn>
2023-12-13 22:51:01 +0700

    Bai tap

--- src/practice/controlcommand/Fibonacci.java
+++ src/practice/controlcommand/Fibonacci.java
@@ -1,7 +1,40 @@
 package practice.controlcommand;
 
 public class Fibonacci {
-    public void printFibonacciNotUseRecursion(int[] numbers) {
-        //TODO
+    /*Viết một chương trình java in ra dãy số Fibonacci không sử dụng đệ quy và có sử dụng đệ quy.
+           Input: 10
+           Output: 0 1 1 2 3 5 8 13 21 34*/
+    /*step1 tạo 1 mảng với kích thước bằng Input
+      step2 duyệt mảng
+      step3  ktr i=0|1, arr[0] và arr[1] = index
+      step4 arr[i] = arr[i-1] + arr[i-2] (i != 0, 1)
+      step5 in */
+    public void printFibonacciNotUseRecursion(int numbers) {
+        int[] arr = new int[numbers];
+        for(int i = 0; i < arr.length; i++){
+            if(i == 0 || i == 1){
+                arr[i] = i;
+                System.out.print(arr[i]+" ");
+            }else{
+                arr[i] = arr[i - 1] + arr[i - 2];
+                System.out.print(arr[i]+" ");
+            }
+        }
     }
+    /*  phương pháp đệ qui
+            step1 duyệt mảng
+            step2 kiểm tra i có bằng 0 || 1
+            step3 nếu bằng return i
+            step4 k bằng return printFibonacciUseRecursion(i -1) + printFibonacciUseRecursion(i - 2)
+            step5 in ra màn hình
+    */
+    public int printFibonacciUseRecursion(int numbers) {
+        int i = numbers;
+            if( i == 0 || i == 1){
+                return i;
+            }else{
+                return printFibonacciUseRecursion(i -1) + printFibonacciUseRecursion(i - 2);
+            }
+    }
+
 }
---
+++ src/practice/controlcommand/ThuatToanChen.java
@@ -0,0 +1,7 @@
+package practice.controlcommand;
+
+public class ThuatToanChen {
+    /*Viết một chương trình java để sắp xếp dãy số bằng cách sử dụng thuật toán chèn(Insertion Sort).
+          Input: 18 9 33 4 84 32
+          Output: 4 9 18 32 33 84*/
+}
